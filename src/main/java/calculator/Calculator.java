e6011031a7d8c5476bbdbc6b391cf907d19c74b5
Lgaigne <97294146+Lgaigne@users.noreply.github.com>
2023-09-27 11:24:40 +0200

    Ajout de test

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,15 @@
 package calculator;
 
 public class Calculator {
+    public int add(int opG, int opD) {
+        int resultatAdd = opG + opD;
+        return resultatAdd;
+
+    }
+
+    public double divide(int opG, int opD){
+        if (opG==0){return 0;}
+        double resultatDiv = opG/opD;
+        return resultatDiv;
+    }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,7 +1,61 @@
 package calculator;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calcul;
+    @BeforeEach
+    void init_Test(){
+        calcul = new Calculator();
+    }
+    @AfterEach
+    void delete_Test(){
+        calcul = null;
+
+    }
+
+    @Test
+    void Test_add() {
+        //GIVEN
+        int opD = 1;
+        int opG = 1;
+
+        //WHEN
+        int resultatAdd = calcul.add(opG,opD);
+
+        //THEN
+        assertThat(resultatAdd).isEqualTo(2);
+    }
+
+    @Test
+    void Test_divide_correct() {
+        //GIVEN
+        Calculator division = new Calculator();
+        int opD = 1;
+        int opG = 1;
+
+        //WHEN
+        double resultatDivBon = calcul.divide(opG,opD);
+
+        //THEN
+        assertThat(resultatDivBon).isEqualTo(1);
+    }
+
+    @Test
+    void Test_divide_0() {
+        //GIVEN
+        int opD = 0;
+        int opG = 0;
+
+        //WHEN
+        double resultatDiv0 = calcul.divide(opG,opD);
+
+        //THEN
+        assertThat(resultatDiv0).isEqualTo(0);
+    }
 
 }
